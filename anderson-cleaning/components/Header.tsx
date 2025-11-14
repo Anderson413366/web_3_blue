0d815a6ba1f39a6b33123961ea12981806920770
Anderson <agomes@andersoncleaning.com>
2025-11-13 23:35:03 -0500

    chore: remove unused Image import from Header

--- anderson-cleaning/components/Header.tsx
+++ anderson-cleaning/components/Header.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
-import Image from 'next/image'
 import { Button } from '@/components/ui/Button'
 import { Menu, X, Moon, Sun } from 'lucide-react'
 import { useTheme } from '@/lib/ThemeProvider'
